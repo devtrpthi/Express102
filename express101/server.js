1c496b31d57a306fe7d2d9789dafb480fcb23e8c
devesh <deveshkrishnatripathi@gmail.com>
2023-01-21 17:41:49 +0530

    muti-requests

--- express101/server.js
+++ express101/server.js
@@ -9,7 +9,20 @@ const  server = http.createServer((req,res) => {
   const homePage = fs.readFileSync('./body.html')
   res.write(homePage)
   res.end()
-   } else {
+   } else if(req.url == '/download.png') {
+  res.writeHead(200,{'content-type': 'text/html'})
+  //res.write('<h1> Hello world!</h1>')
+  const homePage = fs.readFileSync('./download.png')
+  res.write(homePage)
+  res.end()
+  }else if(req.url == '/styles.css') {
+  res.writeHead(200,{'content-type': 'text/css'})
+  //res.write('<h1> Hello world!</h1>')
+  const homePage = fs.readFileSync('./styles.css')
+  res.write(homePage)
+  res.end()
+  }
+  else {
   res.writeHead(404,{'content-type': 'text/html'})
   res.write('<h1> The page u are looking for is not available!</h1>')
   res.end()
