a2f6c04bda1c97773765b43c9a36412ccbcfd3e4
devesh <deveshkrishnatripathi@gmail.com>
2023-01-21 15:18:45 +0530

    serve:static-routing

---
+++ express101/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs')
+
+const  server = http.createServer((req,res) => {
+  //console.log(req)
+  if(req.url == '/') {
+  res.writeHead(200,{'content-type': 'text/html'})
+  //res.write('<h1> Hello world!</h1>')
+  const homePage = fs.readFileSync('./body.html')
+  res.write(homePage)
+  res.end()
+   } else {
+  res.writeHead(404,{'content-type': 'text/html'})
+  res.write('<h1> The page u are looking for is not available!</h1>')
+  res.end()
+  }
+  })
+
+server.listen(3000)
