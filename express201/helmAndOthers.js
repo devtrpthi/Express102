caf2fc8bec7741b2459666b62c8ccd461a34f2ed
devesh <deveshkrishnatripathi@gmail.com>
2023-01-23 12:48:31 +0530

    ajax:request

--- express201/helmAndOthers.js
+++ express201/helmAndOthers.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const helmet = require('helmet')
 
+app.use(helmet({
+   contentSecurityPolicy: false,
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.post('/ajax', (req,res) => {
   console.log(req.body)
-  res.send('Text')
+  res.json('Test')
 })
 
 app.listen(3000)
