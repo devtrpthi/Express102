26516d802abaac364ac800a882f8f1a835cdda44
devesh <deveshkrishnatripathi@gmail.com>
2023-01-23 21:30:04 +0530

    starter

---
+++ express301/loginSite.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const helmet = require('helmet')
+app.use(helmet())
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.set('view engine' , 'ejs')
+app.set('views', path.join(__dirname, 'view'))
+
+app.get('/', (req,res) => {
+  res.render('index')
+})
+
+app.listen(3000)
