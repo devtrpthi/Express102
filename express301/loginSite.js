3e2a8448bc6d9b1b5df9d1072133155096326a9f
devesh <deveshkrishnatripathi@gmail.com>
2023-01-24 20:19:01 +0530

    template:files

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+const helmet = require('helmet')
+app.use(helmet())
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -13,3 +15,5 @@ app.set('views', path.join(__dirname,'views'))
 app.get('/', (req,res) => {
   res.render('index')
 })
+
+app.listen(3000)
