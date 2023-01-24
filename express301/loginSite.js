5f3846073cf807010555bbe193f32bdd9829a6a9
devesh <deveshkrishnatripathi@gmail.com>
2023-01-24 19:51:47 +0530

    modified

---
+++ express301/loginSite.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
+
+app.get('/', (req,res) => {
+  res.render('index')
+})
