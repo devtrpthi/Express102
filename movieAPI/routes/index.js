06730fe057bb8a7eab612e62b21af0856dda4148
devesh <deveshkrishnatripathi@gmail.com>
2023-01-31 02:23:10 +0530

    movies-route

--- movieAPI/routes/index.js
+++ movieAPI/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/most_popular',(req,res,next) => {
+  let page = req.query.page
+  if(page === undefined) {page = 1;}
+  if(req.query.api_key != 123456789){
+    res.json('Invalid Api key')
+  } else{
+  let results =movies.filter((movie) => {
+      return movie.most_popular
+  })
+    const indexToStart = (page-1)*20
+    results = results.slice(indexToStart,indexToStart + 19)
+    res.json({
+      results
+    })
+}})
 
 module.exports = router;
