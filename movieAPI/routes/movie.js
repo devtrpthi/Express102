9adccde64f680d0e5e461fcf3b1b7949012df9b5
devesh <deveshkrishnatripathi@gmail.com>
2023-01-31 02:51:48 +0530

    movie route

--- movieAPI/routes/movie.js
+++ movieAPI/routes/movie.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET movie page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const movieDetails = require('../data/movieDetails')
+
+router.get('/:movieId',(req,res,next) => {
+  const movieId = req.params.movieId
+  const results = movieDetails.find((movie) => {
+    return movie.id == movieId
+  })
+  if(!results){
+    res.json({
+      msg: 'Not found'
+    })
+  }
+  else {
+    res.json({
+      results
+    })
+  }
+})
+
 
 module.exports = router;
