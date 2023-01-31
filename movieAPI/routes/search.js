39b3ce73d3bfaef4f7ddd048e71de940f0492118
devesh <deveshkrishnatripathi@gmail.com>
2023-01-31 20:18:21 +0530

    final commit

--- movieAPI/routes/search.js
+++ movieAPI/routes/search.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies')
+const people = require('../data/people')
+
+function queryRequired(req,res,next) {
+  const searchTerm = req.query.query;
+  if(!searchTerm){
+    res.json({msg: "Query is required."})
+  }else{
+    next()
+  }
+}
+
+router.use(queryRequired)
 /* GET search page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/movie',(req, res, next)=>{
+  const searchTerm = req.query.query;
+  const results = movies.filter((movie)=>{
+    let found = movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+    return found;
+  })
+  res.json({results})
+})
+
+router.get('/person',(req, res, next)=>{
+  const searchTerm = req.query.query;
+  const results = people.filter((person)=>{
+    let found = person.name.includes(searchTerm);
+    return found;
+  })
+  res.json({results})
+})
+
 
 module.exports = router;
